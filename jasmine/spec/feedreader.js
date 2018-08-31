e7e87f3fce0cd0a527925ad4bc07f35851e98414
Charlie Coleman <colemanc@acm.org>
2018-08-31 09:31:16 -0400

    Initial commit RSS Jasmine test suite project

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,72 +1,118 @@
+describe('Feed Reader Testing Project', function() {
 /* feedreader.js
  *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * This is the spec file that Jasmine will read and contains all of the tests that will be run against your application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
-$(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+/* We're placing all of our tests within the $() function, since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready. */
+
+    /* This is our first test suite - a test suite just contains a related set of tests. This suite is all about the RSS
+    * feeds definitions, the allFeeds variable in our application.  */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-        it('are defined', function() {
+        /* This is our first test - it tests to make sure that the allFeeds variable has been defined and that it is not
+         * empty. Experiment with this before you get started on the rest of this project. What happens when you change
+         * allFeeds in app.js to be an empty array and refresh the page?   */
+        it('shows all feeds (in the allFeeds array) are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        /* A test that loops through each feed in the allFeeds object and ensures it has a URL defined 
+         * and that the URL is not empty. */
+        it('shows each feed has a URL and the URL is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe("");
+            });
+        });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        /* A test that loops through each feed in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty.  */
+        it('shows each feed has a name and it is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe("");
+            });
+        });
     });
 
+    describe('The menu', function() {
+        //   this is to check the functioning of the menu
 
-    /* TODO: Write a new test suite named "The menu" */
+        /*   Write a test that ensures the menu element is hidden by default. You'll have to analyze the HTML and
+         * the CSS to determine how we're performing the hiding/showing of the menu element.  */
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        it('initially hides the body (which is the menu) by applying the menu-hidden class', function() {
+            let idiotdesign = $('body');
+            let idiotflag = idiotdesign.hasClass('menu-hidden') ? true : false;
+            expect(idiotflag).toBe(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+         /*   Write a test that ensures the menu changes visibility when the menu icon is clicked. This test
+          * should have two expectations: does the menu display when clicked and does it hide when clicked again.  */
+        it('should toggle menu visibility by clicking menu icon (adding/removing the menu-hidden class to the body div)', function() {
+            let idiot_icon = $('.menu-icon-link');
+            idiot_icon.click();
+            let m_visible = $('body').hasClass('menu-hidden') ? false : true;
+            expect(m_visible).toBe(true);
+            idiot_icon.click();
+            m_visible = $('body').hasClass('menu-hidden') ? false : true;
+            expect(m_visible).toBe(false);
+        });
+    });
+   
+    describe('Initial Entries', function() {
+        /* Write a test that ensures when the loadFeed function is called and completes its work, there is at least
+         * a single .entry element within the .feed container. Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.  */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+        
+        it('upon initial load has at least a single .entry element in the .feed container', function(done) {
+            let feed_div = document.getElementById('whyNoIdHere');
+            feedContent = feed_div.innerHTML;
+            expect(feed_div.childElementCount).toBeDefined();
+            expect(feed_div.childElementCount).not.toBe(0);
+            done();
+        });
+    });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('New Feed Selection', function() {
+        /* Write a test that ensures when a new feed is loaded  by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous. */
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        //   the feedContent variable will be used below to verify feed content changes. The concept is to perform the 
+        // loadFeed with "id 0", store results in feedContent (the .innerHTML). Then do loadFeed with "id 1" and compare
+        // its .innterHTML: it should be different
+        let feedcontent = "";
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+        //   in the previous test suite, the "id 0" feed was loaded and results stored in feedContent (the .innerHTML). So 
+        // here, load "id 1" feed, and compare its .innterHTML; it should be different
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+            let feed_div = document.getElementById('whyNoIdHere');
+            feedContent = feed_div.innerHTML;
+        });
+        beforeEach(function(done) {
+            //   now need to load again because the course material said no test should be dependent on another - otherwise
+            // could have saved the results of Initial Entries to a variable (up a level in scope) to compare. Oh well.
+            loadFeed(1, function() {
+                done();
+            });
+        });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
-}());
+        it('causes new feed content to load when loading a different feed', function(done) {
+            let feed_div = document.getElementById('whyNoIdHere');
+            let newFeedContent = feed_div.innerHTML;
+            expect(newFeedContent).not.toBe(feedContent);
+            done();
+        });
+    });
+});
--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 /* app.js
- *
- * This is our RSS feed reader application. It uses the Google
- * Feed Reader API to grab RSS feeds as JSON object we can make
- * use of. It also uses the Handlebars templating library and
- * jQuery.
- */
+ * This is our RSS feed reader application. It uses the Google Feed Reader API to grab RSS feeds as JSON object we can make
+ * use of. It also uses the Handlebars templating library and jQuery. */
 
 // The names and URLs to all of the feeds we'd like available.
 var allFeeds = [
@@ -23,21 +19,16 @@ var allFeeds = [
     }
 ];
 
-/* This function starts up our application. The Google Feed
- * Reader API is loaded asynchonously and will then call this
- * function when the API is loaded.
- */
+/* This function starts up our application. The Google Feed Reader API is loaded asynchonously and will then call this
+ * function when the API is loaded. */
 function init() {
     // Load the first feed we've defined (index of 0).
     loadFeed(0);
 }
 
-/* This function performs everything necessary to load a
- * feed using the Google Feed Reader API. It will then
- * perform all of the DOM operations required to display
- * feed entries on the page. Feeds are referenced by their
- * index position within the allFeeds array.
- * This function all supports a callback as the second parameter
+/* This function performs everything necessary to load a feed using the Google Feed Reader API. It will then
+ * perform all of the DOM operations required to display  feed entries on the page. Feeds are referenced by their
+ * index position within the allFeeds array.  This function all supports a callback as the second parameter
  * which will be called after everything has run successfully.
  */
  function loadFeed(id, cb) {
@@ -50,7 +41,6 @@ function init() {
        data: JSON.stringify({url: feedUrl}),
        contentType:"application/json",
        success: function (result, status){
-
                  var container = $('.feed'),
                      title = $('.header-title'),
                      entries = result.feed.entries,
@@ -60,18 +50,15 @@ function init() {
                  title.html(feedName);   // Set the header text
                  container.empty();      // Empty out all previous entries
 
-                 /* Loop through the entries we just loaded via the Google
-                  * Feed Reader API. We'll then parse that entry against the
-                  * entryTemplate (created above using Handlebars) and append
-                  * the resulting HTML to the list of entries on the page.
-                  */
+                 /* Loop through the entries we just loaded via the Google Feed Reader API. We'll then parse that entry against the
+                  * entryTemplate (created above using Handlebars) and append the resulting HTML to the list of entries on the page. */
                  entries.forEach(function(entry) {
                      container.append(entryTemplate(entry));
                  });
 
-                 if (cb) {
-                     cb();
-                 }
+                if (cb) {
+                    cb();
+                }
                },
        error: function (result, status, err){
                  //run only the callback without attempting to parse result due to error
@@ -83,15 +70,11 @@ function init() {
      });
  }
 
-/* Google API: Loads the Feed Reader API and defines what function
- * to call when the Feed Reader API is done loading.
- */
+// Google API: Loads the Feed Reader API and defines what function to call when the Feed Reader API is done loading.
 google.setOnLoadCallback(init);
 
-/* All of this functionality is heavily reliant upon the DOM, so we
- * place our code in the $() function to ensure it doesn't execute
- * until the DOM is ready.
- */
+/* All of this functionality is heavily reliant upon the DOM, so we place our code in the $() function to ensure it doesn't execute
+ * until the DOM is ready. */
 $(function() {
     var container = $('.feed'),
         feedList = $('.feed-list'),
@@ -99,12 +82,9 @@ $(function() {
         feedId = 0,
         menuIcon = $('.menu-icon-link');
 
-    /* Loop through all of our feeds, assigning an id property to
-     * each of the feeds based upon its index within the array.
-     * Then parse that feed against the feedItemTemplate (created
-     * above using Handlebars) and append it to the list of all
-     * available feeds within the menu.
-     */
+    /* Loop through all of our feeds, assigning an id property to each of the feeds based upon its index within the array.
+     * Then parse that feed against the feedItemTemplate (created above using Handlebars) and append it to the list of all
+     * available feeds within the menu.  */
     allFeeds.forEach(function(feed) {
         feed.id = feedId;
         feedList.append(feedItemTemplate(feed));
@@ -112,10 +92,8 @@ $(function() {
         feedId++;
     });
 
-    /* When a link in our feedList is clicked on, we want to hide
-     * the menu, load the feed, and prevent the default action
-     * (following the link) from occurring.
-     */
+    /* When a link in our feedList is clicked on, we want to hide the menu, load the feed, and prevent the default action
+     * (following the link) from occurring.  */
     feedList.on('click', 'a', function() {
         var item = $(this);
 
@@ -124,9 +102,7 @@ $(function() {
         return false;
     });
 
-    /* When the menu icon is clicked on, we need to toggle a class
-     * on the body to perform the hiding/showing of our menu.
-     */
+    // When the menu icon is clicked on, we need to toggle a class on the body to perform the hiding/showing of our menu.
     menuIcon.on('click', function() {
         $('body').toggleClass('menu-hidden');
     });
